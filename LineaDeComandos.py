8ae44c5920f5ca1de0daf22fd4337ddb5bafa6f4
Daniel Vega <danielvegac2000@mail.com>
2020-02-26 21:41:04 -0500

    obtiene los conjuntos de la linea de comandos

---
+++ LineaDeComandos.py
@@ -0,0 +1,11 @@
+import sys
+
+obtenerValores = sys.argv[1:]
+
+#print(obtenerValores);
+#modo de escritura.
+#{conjunto1}.{conjunto2}
+
+conjuntos = obtenerValores[0].split("}.{");
+
+print(conjuntos)
