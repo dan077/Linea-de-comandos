fa836b90dedfab8691830635a3101176bc2a0b32
Daniel Vega <danielvegac2000@mail.com>
2020-02-27 00:17:33 -0500

    prueba de metodos para la clase.
    Hacia pruebas para observar si los metodos de la clase funcionaban correctamente.

--- LineaDeComandos.py
+++ LineaDeComandos.py
@@ -1,14 +1,21 @@
 import sys
-
+from conjuntos import  Conjunto
 obtenerValores = sys.argv[1:]
 
 #print(obtenerValores);
 #modo de escritura.
 #{conjunto1}.{conjunto2}
 #hol
+
 conjuntos = obtenerValores[0].split("}.{");
 
-print(conjuntos)
+conjunto1 = Conjunto(["1","2","6","4"],4);
+conjunto2 = Conjunto(["3","2","5","7"],4);
+
+print(conjunto1.getUnion(conjunto2))
+
+
+#print(conjuntos)
 
 """@andres te toca."""
 #def separarConjuntos(array):
