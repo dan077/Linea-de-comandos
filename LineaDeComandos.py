0632f6b7a4bbdccc4db81f2fa363fc3e7fcdaa1f
Daniel Vega <danielvegac2000@mail.com>
2020-02-27 17:20:23 -0500

    Crea los objetos tipo conjunto.
    los conjuntos ya son almacenados  en un listObject por lo que se da por culminado  el procesamiento del argumento ingresado.

--- LineaDeComandos.py
+++ LineaDeComandos.py
@@ -12,6 +12,8 @@ conjuntos = obtenerValores[0].split("}.{");
 conjunto1 = Conjunto(["1","2","6","4"],4);
 conjunto2 = Conjunto(["3","2","5","7"],4);
 
+conjuntosProcesados = [];
+
 print(conjunto1.getUnion(conjunto2))
 
 
@@ -25,5 +27,14 @@ def separarConjuntos(conjuntos):
         conjuntos[i] = conjuntos[i].replace("}", "")
     return conjuntos
 
+def CrearConjuntos(conjuntos):
+    listObj = []
+    for i in conjuntos:
+        elementosDelConjunto = i.split(",")
+        listObj.append(Conjunto(elementosDelConjunto,len(elementosDelConjunto)));
+    return listObj;
+
 a = separarConjuntos(conjuntos)
+b = CrearConjuntos(conjuntos)
 print(conjuntos)
+print(b[0].getUnion(b[1]))
