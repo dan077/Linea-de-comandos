bf16e3be2526a175237a0a0780f8f00fb82b6376
Daniel Vega <danielvegac2000@mail.com>
2020-02-27 17:38:53 -0500

    El codigo ordenado
    el codigo ya ordenado xd

--- LineaDeComandos.py
+++ LineaDeComandos.py
@@ -1,40 +1,25 @@
 import sys
 from conjuntos import  Conjunto
+
 obtenerValores = sys.argv[1:]
-
-#print(obtenerValores);
-#modo de escritura.
-#{conjunto1}.{conjunto2}
-#hol
-
-conjuntos = obtenerValores[0].split("}.{");
-
-conjunto1 = Conjunto(["1","2","6","4"],4);
-conjunto2 = Conjunto(["3","2","5","7"],4);
-
-conjuntosProcesados = [];
-
-print(conjunto1.getUnion(conjunto2))
-
-
-#print(conjuntos)
 
 
 """@andres te toca."""
-def separarConjuntos(conjuntos):
+def SepararConjuntos(texto):
+    conjuntos = texto.split("}.{");
     for i in range(len(conjuntos)):
         conjuntos[i] = conjuntos[i].replace("{", "")
         conjuntos[i] = conjuntos[i].replace("}", "")
     return conjuntos
 
-def CrearConjuntos(conjuntos):
+def ProcesarConjuntos(conjuntos):
     listObj = []
     for i in conjuntos:
         elementosDelConjunto = i.split(",")
         listObj.append(Conjunto(elementosDelConjunto,len(elementosDelConjunto)));
     return listObj;
 
-a = separarConjuntos(conjuntos)
-b = CrearConjuntos(conjuntos)
-print(conjuntos)
-print(b[0].getUnion(b[1]))
+conjuntosSeparados = SepararConjuntos(obtenerValores[0])
+conjuntosProcesados = ProcesarConjuntos(conjuntosSeparados)
+print(conjuntosSeparados);
+print(conjuntosProcesados[0].getUnion(conjuntosProcesados[1]))
