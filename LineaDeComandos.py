21afea0b54bbfb5aa30401d2a7ee4ade34bc047f
Daniel Vega <danielvegac2000@mail.com>
2020-02-28 16:12:31 -0500

    mejoras.
    se añadieron las operaciones de conjuntos "diferencia" e "interseccion". reduje el codigo por lo que creo que está mejor.

--- LineaDeComandos.py
+++ LineaDeComandos.py
@@ -3,10 +3,17 @@ from conjuntos import  Conjunto
 
 obtenerValores = sys.argv[1:]
 
+diccionario = {"union":["union","}.{"],"diferencia":["diferencia","}-{"],"interseccion":["interseccion","}/{"]}
+
+operacion = diccionario["union"][0] if diccionario["union"][1] in obtenerValores[0] else \
+                diccionario["diferencia"][0] if diccionario["diferencia"][1] in obtenerValores[0] else\
+                    diccionario["interseccion"][0] if diccionario["interseccion"][1] in obtenerValores[0] else "null";
+
+print("la operacion es "+ operacion);
 
 """@andres te toca."""
-def SepararConjuntos(texto):
-    conjuntos = texto.split("}.{");
+def SepararConjuntos(texto,operacion):
+    conjuntos = texto.split(diccionario.get(operacion)[1]); #interseccion
     for i in range(len(conjuntos)):
         conjuntos[i] = conjuntos[i].replace("{", "")
         conjuntos[i] = conjuntos[i].replace("}", "")
@@ -19,7 +26,16 @@ def ProcesarConjuntos(conjuntos):
         listObj.append(Conjunto(elementosDelConjunto,len(elementosDelConjunto)));
     return listObj;
 
-conjuntosSeparados = SepararConjuntos(obtenerValores[0])
-conjuntosProcesados = ProcesarConjuntos(conjuntosSeparados)
-print(conjuntosSeparados);
-print(conjuntosProcesados[0].getUnion(conjuntosProcesados[1]))
+"""def ImprimirConjuntos(conjuntos):
+    for i in conjuntos:
+        print(i);"""
+
+def RealizarOperacion(conjuntos,operacion):
+    return eval("conjuntos[0].get{0}(conjuntos[1])".format(operacion.capitalize()));
+
+
+if(diccionario.get(operacion)):
+    conjuntosSeparados = SepararConjuntos(obtenerValores[0], operacion)
+    conjuntosProcesados = ProcesarConjuntos(conjuntosSeparados)
+    #ImprimirConjuntos(conjuntosProcesados)
+    print(RealizarOperacion(conjuntosProcesados,operacion));
